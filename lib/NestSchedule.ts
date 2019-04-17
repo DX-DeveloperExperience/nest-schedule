6293e578b08a73624025622fb3e12b9db7ac11d8
TERRIER Bastien <bastien.terrier@gmail.com>
2019-04-17 16:07:07 +0200

    can update a Cron job

--- lib/NestSchedule.ts
+++ lib/NestSchedule.ts
@@ -1,7 +1,12 @@
-import { NestDistributedSchedule } from "./NestDistributedSchedule";
+import { NestDistributedSchedule } from './NestDistributedSchedule';
 
 export class NestSchedule extends NestDistributedSchedule {
-    async tryLock(method: string): Promise<any> {
-        return false;
-    }
+  async tryLock(method: string): Promise<any> {
+    return false;
+  }
+  updateCron(cron: string) {
+    this.cronJobs.forEach(c => {
+      c.reschedule(cron);
+    });
+  }
 }
